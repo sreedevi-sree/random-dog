e0cb9803f9b0ed8bd656034341fc231a3c5bd1b8
sreedevi-sree <sreedevifsd@gmail.com>
2021-11-12 15:15:11 +0530

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const cat_btn = document.getElementById("cat_btn");
-const cat_result = document.getElementById("cat_result");
-
-cat_btn.addEventListener("click", getRandomCat);
+const dog_btn = document.getElementById("dog_btn");
+const dog_result = document.getElementById("dog_result");
 
 
-function getRandomCat() {
-  fetch("https://aws.random.cat/meow")
+dog_btn.addEventListener("click", getRandomDog);
+
+
+function getRandomDog() {
+  fetch("https://random.dog/woof.json")
     .then((res) => res.json())
     .then((data) => {
-      cat_result.innerHTML = `<img src=${data.file}  width="300px" height="300px" alt="cat"/>`;
+      if (data.url.includes(".mp4")) {
+        getRandomDog();
+      } else {
+        dog_result.innerHTML = `<img src=${data.url} width="300px" height="300px" alt="dog"/>`;
+      }
     });
 }
-
